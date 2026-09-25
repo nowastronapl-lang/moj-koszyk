45a9faac3c53cc714f4e5c7772a74b0083049365
nowastronapl-lang <nowastronapl@gmail.com>
2026-09-25 19:32:23 +0200

    Fix GitHub Pages basePath configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubActions ? '/moj-koszyk' : '',
+  assetPrefix: isGithubActions ? '/moj-koszyk/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
